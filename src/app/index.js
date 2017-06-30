820d149166b7049cb7b56331d030273c6ca4f4ba
Juan Fernandez-Corugedo <jfcorugedo@gmail.com>
2017-06-30 22:23:49 +0200

    extract inputs and button to a new component: TimerAdder

--- src/app/index.js
+++ src/app/index.js
@@ -1,41 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
+import TimerAdder from './timer-adder';
 import './index.css';
 
-class App extends Component {
+const App = () => {
+    return (
+        <div className="app">
+            <TimerAdder />
+        </div>
+    );
+};
 
-    constructor() {
-        super();
-        this.state = {timerName: '', timerTime: 0};
-    }
-
-    updateTimerName(event) {
-        this.setState({timerName: event.target.value});
-    }
-
-    updateTimerTime(event) {
-        this.setState({timerTime: event.target.value});
-    }
-
-    createTimer(event) {
-        alert(this.state.timerName + ' ' + this.state.timerTime);
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <div className="timer-adder">
-                    <form onSubmit={(event) => this.createTimer(event)}>
-                        <input type="text" name="timerName" placeholder="timer name"
-                               onChange={(event) => this.updateTimerName(event)}/>
-                        <input type="text" name="" placeholder="time in seconds"
-                               onChange={(event) => this.updateTimerTime(event)}/>
-                        <button type="submit">Create</button>
-                    </form>
-                </div>
-            </div>
-        );
-    }
-}
 
 export default App;
---
+++ src/app/timer-adder/index.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class TimerAdder extends Component {
+
+    constructor() {
+        super();
+        this.state = {timerName: '', timerTime: 0};
+    }
+
+    updateTimerName(event) {
+        this.setState({timerName: event.target.value});
+    }
+
+    updateTimerTime(event) {
+        this.setState({timerTime: event.target.value});
+    }
+
+    createTimer(event) {
+        alert(this.state.timerName + ' ' + this.state.timerTime);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="timer-adder">
+                <form onSubmit={(event) => this.createTimer(event)}>
+                    <input type="text" name="timerName" placeholder="timer name"
+                           onChange={(event) => this.updateTimerName(event)}/>
+                    <input type="text" name="" placeholder="time in seconds"
+                           onChange={(event) => this.updateTimerTime(event)}/>
+                    <button type="submit">Create</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default TimerAdder;
